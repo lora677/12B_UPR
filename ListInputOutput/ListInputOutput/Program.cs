f4905021504ccdf718546d6bce5f5282df1d5918
lora677 <l.kirilova2017@pmg-pernik.com>
2021-09-30 12:50:25 +0300

    Comm

---
+++ ListInputOutput/ListInputOutput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListInputOutput
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+ 
+        {
+            int n = int.Parse(Console.ReadLine());
+            List <int> list = new List<int> ();
+            for (int i = 0; i  < n; i++) {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int index = 0; index < list.Count; index++) {
+                Console.WriteLine("list[{0}] = {1}", index, list[index]);
+            }
+        }
+    }
+}
